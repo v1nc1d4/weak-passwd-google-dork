80c6f1040daf850dc4a14b52d854ba2ae466705b
Lofer-1 <115648561+Lofer-1@users.noreply.github.com>
2022-10-12 22:01:45 +0530

    Update Clock_GUI.py

--- Clock_GUI.py
+++ Clock_GUI.py
@@ -14,11 +14,5 @@ def window(self):
 
 clock_label = Label(window, bg="dark red", fg="white", font = ("Times", 100, 'bold'), relief='flat')
 clock_label.place(x = 20, y = 20)
-
-def update_label():
-    current_time = strftime('%H: %M: %S')
-    clock_label.configure(text = current_time)
-    clock_label.after(80, update_label)
-
 update_label()
 window.mainloop()
