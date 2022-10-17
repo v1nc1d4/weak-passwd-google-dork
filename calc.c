ec792bddb4aafddcddd40c0256e90591ab947c8f
Jestin777 <115986608+Jestin777@users.noreply.github.com>
2022-10-17 15:10:27 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -19,9 +19,7 @@ int main() {
     case '*':
       printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
       break;
-    case '/':
-      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-      break;
+   
     // operator doesn't match any case constant
     default:
       printf("Error! operator is not correct");
